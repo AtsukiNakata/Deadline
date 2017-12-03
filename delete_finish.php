7e18dfe0e6b219b623b680b121a68cfd47cccc9a
AtsukiNakata <naka.atsu0804@gmail.com>
2017-12-03 10:37:31 +0900

    local commit

---
+++ delete_finish.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="style07.css">
+		<script src="jquery-3.2.1.min.js"></script>
+		<script src="kadai_07.js"></script>
+    <title>課題の管理</title>
+	</head>
+
+	<body>
+		<header>
+			<h3>課題の管理</h3>
+		</header>
+
+    <div class = "container-left">
+			<nav id = "g_navi">
+				<ul id = "navi">
+					<li><a href="home_kadai07.php" class = "btn" id = "menu1">ホーム画面</a></li>
+					<li><a href="form_kadai.php" class = "btn" id = "menu2">課題の追加</a></li>
+					<li><a href="sent_07.php" class = "btn" id = "menu3">課題一覧</a></li>
+					<li><a href="delete_07.php" class = "btn" id = "menu4">課題の削除</a></li>
+				</ul>
+			</nav>
+		</div>
+			<div class="container-right">
+        <div class = "main1">
+          <p>課題の削除に成功しました。</p>
+        </div>
+			</div>
+	</body>
+
+</html>
---
+++ home_kadai07.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="style07.css">
+    <title>課題の管理</title>
+	</head>
+
+	<body>
+		<header>
+			<h3>課題の管理</h3>
+		</header>
+
+    <div class = "container-left">
+			<nav id = "g_navi">
+				<ul id = "navi">
+					<li><a href="home_kadai07.php" class = "btn" id = "menu1">ホーム画面</a></li>
+					<li><a href="form_kadai.php" class = "btn" id = "menu2">課題の追加</a></li>
+					<li><a href="sent_07.php" class = "btn" id = "menu3">課題一覧</a></li>
+					<li><a href="delete_07.php" class = "btn" id = "menu4">課題の削除</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		<style type = "text/css">
+			#menu1{
+				color:orange;
+			}
+		</style>
+
+			<div class="container-right">
+        <div class = "main1">
+          <P>これはホーム画面です。</p>
+        </div>
+			</div>
+	</body>
+
+</html>
